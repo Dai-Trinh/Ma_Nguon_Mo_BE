3639a2f64a68716996a21fcde39646cf8e933269
Dai-Trinh <97729352+Dai-Trinh@users.noreply.github.com>
2023-07-22 13:49:43 +0700

    pham mem ma nguon mo be

---
+++ src/main/java/com/example/manguonmo_be/ManguonmoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.manguonmo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManguonmoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManguonmoBeApplication.class, args);
+	}
+
+}
